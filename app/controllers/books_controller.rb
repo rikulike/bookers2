17e392dbea27f27039070d8d74b8a6823dbe3871
RikuTakayama <ec2-user@ip-172-31-24-155.ap-northeast-1.compute.internal>
2024-09-06 17:27:39 +0000

    9/7 [add] header createアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,13 @@
 class BooksController < ApplicationController
+  
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to user_path(current_user.id)
+  end
+  
+  
   def index
     @book = Book.new
     @user = current_user
@@ -10,5 +19,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
 
   def show
     @book = Book.new
+    @user=User.find(params[:id])
+    @books=@user.books
   end
 
   def edit
